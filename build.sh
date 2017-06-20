db2f81e2a40ef3190244661349b5c2bf87e5df76
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-06-20 14:36:56 +0200

    build script

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ FOLDER="$(cd "$(dirname "$0")" && pwd)"
 rm -rf ${FOLDER}/env
 conda create --yes -p ${FOLDER}/env python=3.6.0 pandas matplotlib
 
+# install cvxpy
+conda install --yes -c cvxgrp cvxpy
+
 # pip install -r requirements.txt
-# install cvxpy?
